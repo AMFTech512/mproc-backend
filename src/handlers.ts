e7a854d1184b329e64ab8323ec9b430ca5bcb088
Austin <austin@austinfay.com>
2024-03-16 14:04:11 -0600

    fix: file cleanup after request

--- src/handlers.ts
+++ src/handlers.ts
@@ -3,6 +3,7 @@ import { DIContainer } from "./di";
 import { ProcessStep, getBuffer, processSteps } from "./image-processor";
 import { ValidationError } from "joi";
 import gm from "gm";
+import { rm } from "fs/promises";
 
 // POST /upload
 export const handleUpload: (container: DIContainer) => RequestHandler =
@@ -21,6 +22,9 @@ export const handleUpload: (container: DIContainer) => RequestHandler =
         res
           .status(400)
           .send("`processSteps` must be a valid JSON array of process steps.");
+
+        // delete the file from the file system regardless of whether or not the request was successful
+        if (req.file?.path) await rm(req.file.path);
         return;
       }
 
@@ -39,11 +43,12 @@ export const handleUpload: (container: DIContainer) => RequestHandler =
           res.status(500).send("A server error occurred.");
         }
       }
-
-      // TODO: delete the file from the file system
     } else {
       // if there is no file, send a 400 error
       // TODO: send a more descriptive error message
       res.sendStatus(400);
     }
+
+    // delete the file from the file system regardless of whether or not the request was successful
+    if (req.file?.path) await rm(req.file.path);
   };
